8e685fc29eb6e21117264d8dfdb452cc2b772614
ElCarambola <yakovchikdenis@gmail.com>
2025-02-18 19:55:00 +0300

    setting up a project

---
+++ src/db.js
@@ -0,0 +1,21 @@
+const date1 = new Date(2025, 1, 12, 10, 24);
+const date2 = new Date(2025, 1, 14, 14, 47);
+
+export const todos = [
+    {
+        title: 'Изучить React',
+        desc: 'Да поскорее!',
+        image: '',
+        done: true,
+        createAt: date1.toLocaleString(),
+        key: date1.getTime(),
+    },
+    {
+        title: 'Написать первое React-приложение',
+        desc: 'Список запланированных дел',
+        image: '',
+        done: false,
+        createAt: date2.toLocaleString(),
+        key: date2.getTime(),
+    }
+];
